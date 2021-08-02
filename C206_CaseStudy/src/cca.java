ad4a594ad07d6121b217c7e03ce2655c2a514445
nxcoletan <nxcoletan>
2021-08-02 21:17:05 +0800

    done

--- C206_CaseStudy/src/cca.java
+++ C206_CaseStudy/src/cca.java
@@ -11,5 +11,67 @@
  */
 
 public class cca {
-
+	private String title;
+	private String description;
+	private Integer size;
+	private String day;
+	private String time;
+	private String venue;
+	private String InstructorName;
+	
+	
+	public cca(String title, String description, Integer size, String day, String time, String venue,
+			String instructorName) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.size = size;
+		this.day = day;
+		this.time = time;
+		this.venue = venue;
+		InstructorName = instructorName;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getSize() {
+		return size;
+	}
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	public String getInstructorName() {
+		return InstructorName;
+	}
+	public void setInstructorName(String instructorName) {
+		InstructorName = instructorName;
+	}
 }
