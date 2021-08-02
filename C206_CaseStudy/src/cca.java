5c497cf0a459c7975b13616f08e967bf9f13a1fe
nxcoletan <nxcoletan>
2021-08-02 21:16:42 +0800

    done

---
+++ C206_CaseStudy/src/cca.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Nicole
+ * Student ID: 20025835
+ * Class: E37A
+ * Date/Time created: Monday 02-08-2021 21:02
+
+ */
+
+public class cca {
+
+}
